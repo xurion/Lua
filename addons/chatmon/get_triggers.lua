b4b1c4b8fcc0cc431889cb06d02fd65407a02c17
Nifim <TesaRollens@gmail.com>
2024-10-28 19:12:40 -0700

    fix check for player specific file

--- addons/chatmon/get_triggers.lua
+++ addons/chatmon/get_triggers.lua
@@ -1,11 +1,11 @@
 local get_triggers = {}
 
 function get_triggers.by_name(name)
-    local player_path = windower.addon_path .. "chatmon/data/triggers/" .. name .. ".lua"
+    local player_path = windower.addon_path .. "/data/triggers/" .. name .. ".lua"
     local triggers_loader
     if windower.file_exists(player_path) then
         print('ChatMon: Loading /triggers/' .. name .. '.lua')
-        triggers_loader = loadfile(windower.addon_path .. "/data/triggers/" .. name .. ".lua")
+        triggers_loader = loadfile(player_path)
     else
         print('ChatMon: Loading global triggers ')
         triggers_loader = loadfile(windower.addon_path .. "/data/triggers/global.lua")
